4bcd1c51249727c622c2c761f5932b71b17c33a9
Cameron Pavey <cpave3@gmail.com>
2022-10-31 15:57:34 +1100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Box maxW="32rem">
+      <Heading mb={4}>Chakra UI Demo</Heading>
+      <Text fontSize="xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias excepturi
+        voluptas numquam assumenda debitis facere nobis fuga quis laborum quia
+        sunt inventore tenetur illo dolorem perferendis, ad id ex eaque.
+      </Text>
+      <Button size="lg" color={"brand.600"} mt="24px">
+        Do something fun today
+      </Button>
+    </Box>
+  );
+}
+
+export default App;
